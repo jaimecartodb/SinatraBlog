629b73d197f281d738029bd13380af07ec927ec9
jaimecartodb <jdemora@cartodb.com>
2015-11-28 08:15:10 -0500

    everything is working

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,13 @@
 require 'Date'
 
 class Post
-  attr_accessor :title, :date, :text
+  attr_accessor :title, :date, :text, :category, :author
 
-  def initialize title, date, text
+  def initialize title, date, text, category = nil, author = nil
     @title = title
     @date = date
     @text = text
     @category = category
     @author = author
   end
-
 end
